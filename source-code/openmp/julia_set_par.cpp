fdb82c2bbe9a96e5c5ea1190e0b6780e96e4fe8e
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-05-09 21:54:39 +0200

    Initial commit

---
+++ source-code/openmp/julia_set_par.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <execution>
+#include <iostream>
+#include <valarray>
+
+using namespace std;
+using NanoTime = chrono::nanoseconds;
+
+using Complex = complex<float>;
+
+valarray<float> coordinates(float min_coord, float max_coord,
+                             size_t steps);
+valarray<Complex> z_values(const valarray<float>& x_coords,
+                         const valarray<float>& y_coords);
+valarray<int> iterate_zs(valarray<Complex>& zs, const Complex& c,
+                         size_t max_iters);
+void print_results(const valarray<int>& ns);
+
+int main(int argc, char *argv[]) {
+    const Complex c(-0.62772f, - 0.42193f);
+    const float x1 {-1.8f};
+    const float x2 {1.8f};
+    const float y1 {-1.8f};
+    const float y2 {1.8f};
+    const size_t max_iters {255};
+    size_t steps {100};
+    if (argc > 1)
+        steps = stoi(argv[1]);
+    valarray<float> x_coords = coordinates(x1, x2, steps);
+    valarray<float> y_coords = coordinates(y1, y2, steps);
+    valarray<Complex> zs = z_values(x_coords, y_coords);
+    auto start_time = chrono::steady_clock::now();
+    valarray<int> ns = iterate_zs(zs, c, max_iters);
+    auto end_time = chrono::steady_clock::now();
+    auto duration = chrono::duration_cast<NanoTime>(end_time - start_time);
+    cerr << "time: " << duration.count()*1.0e-9 << " s" << endl;
+    print_results(ns);
+    return 0;
+}
+
+valarray<float> coordinates(float min_coord, float max_coord,
+                             size_t steps) {
+    valarray<float> coords(steps);
+    const float step {(max_coord - min_coord)/steps};
+    float value {min_coord};
+    for (size_t i = 0; i < steps; i++) {
+        coords[i] = value;
+        value += step;
+    }
+    return coords;
+}
+
+valarray<Complex> z_values(const valarray<float>& x_coords,
+                         const valarray<float>& y_coords) {
+    valarray<Complex> zs(x_coords.size()*y_coords.size());
+    size_t i {0};
+    for (auto y: y_coords)
+        for (auto x: x_coords) {
+            Complex z(x, y);
+            zs[i++] = z;
+        }
+    return zs;
+}
+
+int iterate_z(Complex& z, const Complex& c, size_t max_iters) {
+    size_t n {0};
+    while (real(z)*real(z) + imag(z)*imag(z) < 4.0f && n++ < max_iters)
+        z = z*z + c;
+    return n;
+}
+
+valarray<int> iterate_zs(valarray<Complex>& zs, const complex<float>& c,
+                         size_t max_iters) {
+    valarray<int> ns(zs.size());
+    std::transform(std::execution::par,
+            std::begin(zs), std::end(zs), std::begin(ns),
+            [=] (Complex& z) { return iterate_z(z, c, max_iters); });
+    return ns;
+}
+
+void print_results(const valarray<int>& ns) {
+    size_t steps = (static_cast<size_t>(sqrt(ns.size()) + 0.1f));
+    size_t count {0};
+    for (auto n: ns) {
+        cout << n;
+        if (++count % steps == 0)
+            cout << endl;
+        else
+            cout << " ";
+    }
+}
---
+++ source-code/openmp/vector_product_cpu.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define NX 204800000
+
+int main()
+{
+    float *vecA, *vecB, *vecC;
+    vecA = (float *) malloc(NX*sizeof(float));
+    vecB = (float *) malloc(NX*sizeof(float));
+    vecC = (float *) malloc(NX*sizeof(float));
+    if (vecA == NULL || vecB == NULL || vecC == NULL) {
+        fprintf(stderr, "error: can not allocated array of size %d\n", NX);
+        return 1;
+    }
+
+    float r = 0.2f;
+
+    /* Initialization of vectors */
+    for (int i = 0; i < NX; i++) {
+        vecA[i] = pow(r, i);
+        vecB[i] = 1.0f;
+        vecC[i] = 0.0f;
+    }
+
+    /* dot product of two vectors */
+#pragma omp parallel for
+    for (int i = 0; i < NX; i++) {
+        for (int j = 0; j < 10000; j++) {
+            vecC[i] += vecA[i] * vecB[i];
+        }
+    }
+
+    float sum = 0.0;
+    /* calculate the sum */
+    for (int i = 0; i < NX; i++) {
+        sum += vecC[i];
+    }
+    printf("The sum is: %8.6f \n", sum);
+    free(vecA);
+    free(vecB);
+    free(vecC);
+
+    return 0;
+}
