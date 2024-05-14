af697dc385a38b7fa9becea7c81888ce4c6d86e3
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-05-14 09:01:27 +0200

    Add workaround for out-of-memory gnuplot issue

--- source-code/openmp/plot_julia.sh
+++ source-code/openmp/plot_julia.sh
@@ -13,11 +13,16 @@ y_size=$(wc -l $file | cut -d ' ' -f 1)
 words=$(wc -w $file | cut -d ' ' -f 1)
 x_size=$(( $words / $y_size ))
 
+echo "### info: image size: $x_size x $y_size"
+
+ulimit -s 65536
+
 # plot the Julia set
 gnuplot -persist <<- EOI
+    plot "$file" matrix with image
     set size square
     unset key
     set xrange [0:$(( $x_size - 1))]
     set yrange [0:$(( $y_size - 1))]
-    plot "$file" matrix with image
+    replot
 EOI
