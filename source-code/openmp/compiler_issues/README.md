70c4cb182a0ed5e463389a1161da3229971a9679
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-05-28 13:13:33 +0200

    Add example of allocating memory directly on device

--- source-code/openmp/compiler_issues/README.md
+++ source-code/openmp/compiler_issues/README.md
@@ -1,11 +1,13 @@
 # Compiler issues
 
-Unfortunately, the NVIDIA compiler (24.3) can compile the code in this directory.
+Unfortunately, the NVIDIA compiler (24.3) can compile the code in this
+directory.
 
 
 ## What is it?
 
 1. `julia_omp_target_declare.c`: to call functions in a target region, the
-    functions need to be declared with `omp declare target` in the source
-    file.
+   functions need to be declared with `omp declare target` in the source file.
+1. `device_allocation_gpu.c`: example of allocating memory on the GPU directly
+   using omp_target_alloc.
 1. `CMakeLists.txt`: CMake file to build the application.
---
+++ source-code/openmp/compiler_issues/device_allocation_gpu.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+int main(int argc, char *argv[]) {
+    const int n = argc > 1 ? atoi(argv[1]) : 1000;
+    const int nr_iters = argc > 2 ? atoi(argv[2]) : 10;
+    float *a = (float *) malloc(n*n*sizeof(float));
+    if (!a) {
+        fprintf(stderr, "malloc failed\n");
+        return 1;
+    }
+#pragma omp target enter data map(to:a[0:n*n])
+#pragma omp target teams distribute parallel for
+    for (int i = 0; i < n; i++) {
+        a[i*n + i] = 0.0f;
+    } 
+    float *b = (float *) omp_target_alloc(n*n*sizeof(float), omp_get_default_device());
+    if (!b) {
+        fprintf(stderr, "omp_target_alloc failed\n");
+        return 1;
+    }
+#pragma omp target teams distribute parallel for is_device_ptr(b)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            b[i*n + j] = 2.0f*(rand()/(float) RAND_MAX - 0.5f);
+        }
+    }
+    for (int iter = 0; iter < nr_iters; iter++) {
+#pragma omp target teams distribute parallel for is_device_ptr(b)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i*n + j] += b[i*n + j];
+            }
+        }
+    }
+    omp_target_free(b, omp_get_default_device());
+#pragma omp target exit data map(from:a[0:n*n])
+    float sum = 0.0f;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            sum += a[i*n + j];
+        }
+    }
+    printf("sum = %f\n", sum);
+    free(a);
+    return 0;
+}
