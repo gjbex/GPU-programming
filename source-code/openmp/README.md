d802dad86a11bb3c3f45863c787f0a5ab585afa0
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-05-28 12:22:52 +0200

    Add example of unstructured data region

--- source-code/openmp/README.md
+++ source-code/openmp/README.md
@@ -19,6 +19,8 @@ HPC SDK](https://developer.nvidia.com/hpc-sdk) is used.
    on the CPU.
 1. `julia_omp_cpu.c`: application that computes a Julia set on the CPU.
 1. `julia_omp_gpu.c`: application that computes a Julia set on the GPU.
+1. `unstructured_data_region_gpu.c`: application that demonstrates
+   the use of the `target` directive with an unstructured data region.
 1. `CMakeLists.txt`: CMake file to build the applications.
 1. `compiler_issues`: directory that contains example that can not be
    compiled with the NVIDIA HPC SDK compiler.
---
+++ source-code/openmp/unstructured_data_region_gpu.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+int main(int argc, char *argv[]) {
+    const int n = argc > 1 ? atoi(argv[1]) : 1000;
+    const int nr_iters = argc > 2 ? atoi(argv[2]) : 10;
+    float *a = (float *) malloc(n*n*sizeof(float));
+#pragma omp target enter data map(to:a[0:n*n])
+#pragma omp target teams distribute parallel for
+    for (int i = 0; i < n; i++) {
+        a[i*n + i] = 0.0f;
+    } 
+    float *b = (float *) malloc(n*n*sizeof(float));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            b[i*n + j] = 2.0f*(rand()/(float) RAND_MAX - 0.5f);
+        }
+    }
+#pragma omp target enter data map(to:b[0:n*n])
+    for (int iter = 0; iter < nr_iters; iter++) {
+#pragma omp target teams distribute parallel for
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i*n + j] += b[i*n + j];
+            }
+        }
+    }
+#pragma omp target exit data map(from:a[0:n*n])
+    float sum = 0.0f;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            sum += a[i*n + j];
+        }
+    }
+    printf("sum = %f\n", sum);
+    free(a);
+    return 0;
+}
