ed2ac66864d08d56f6d55f2d303d6345bc5d073b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-07-12 11:25:05 +0200

    Make code more flexible

--- source-code/openmp/vector_product_cpu.c
+++ source-code/openmp/vector_product_cpu.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define NX 2048000
 
-int main()
-{
+int main(int argc, char *argv[]) {
+    const int n = argc > 1 ? atoi(argv[1]) : 10000;
+    const int N = argc > 2 ? atoi(argv[2]) : 2048000;
     float *vecA, *vecB, *vecC;
-    vecA = (float *) malloc(NX*sizeof(float));
-    vecB = (float *) malloc(NX*sizeof(float));
-    vecC = (float *) malloc(NX*sizeof(float));
+    vecA = (float *) malloc(N*sizeof(float));
+    vecB = (float *) malloc(N*sizeof(float));
+    vecC = (float *) malloc(N*sizeof(float));
     if (vecA == NULL || vecB == NULL || vecC == NULL) {
-        fprintf(stderr, "error: can not allocated array of size %d\n", NX);
+        fprintf(stderr, "error: can not allocated array of size %d\n", N);
         return 1;
     }
 
-    float r = 0.2f;
-
-    /* Initialization of vectors */
-    for (int i = 0; i < NX; i++) {
-        vecA[i] = pow(r, i);
-        vecB[i] = 1.0f;
+    const float r = 0.2f;
+    for (int i = 0; i < N; i++) {
+        vecA[i] = r*i;
+        vecB[i] = 2.1f;
         vecC[i] = 0.0f;
     }
 
-    /* dot product of two vectors */
-#pragma omp parallel for
-    for (int i = 0; i < NX; i++) {
-        for (int j = 0; j < 10000; j++) {
-            vecC[i] += vecA[i] * vecB[i];
+#pragma omp parallel for default(none) shared(vecA, vecB, vecC, n, N)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < n; j++) {
+            vecC[i] += vecA[i]*vecB[i];
         }
     }
 
-    float sum = 0.0;
-    /* calculate the sum */
-    for (int i = 0; i < NX; i++) {
+    float sum = 0.0f;
+    for (int i = 0; i < N; i++) {
         sum += vecC[i];
     }
-    printf("The sum is: %8.6f \n", sum);
+    printf("The sum is: %8.6e \n", sum);
+
     free(vecA);
     free(vecB);
     free(vecC);
