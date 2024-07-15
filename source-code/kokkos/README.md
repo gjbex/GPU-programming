597f9ccd8455b5c591cc9c033052c59da3857fc2
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-07-15 16:56:27 +0200

    Add example of deep copy and non-trivial reduction

--- source-code/kokkos/README.md
+++ source-code/kokkos/README.md
@@ -10,4 +10,6 @@ several backends such as OpenMP, CUDA, HIP, and SYCL.
    `Kokkos::paralle_for`.
 1. `vector_product.cpp`: vector multiplication using classes.
 1. `vector_product_lambdas.cpp`: vector multiplication using lambdas.
-
+1. `random_numbers.cpp`: illustrates deep copy of views and reduction
+   to a non-scalar.
+1. `CMakeLists.txt`: CMake file to build the applications.
---
+++ source-code/kokkos/random_numbers.cpp
@@ -0,0 +1,56 @@
+#include <Kokkos_Core.hpp>
+#include <iostream>
+#include <random>
+#include <vector>
+
+const int nr_distr {5};
+using view_type = Kokkos::View<float * [nr_distr]>;
+using host_view_type = view_type::HostMirror;
+
+struct MultiSum {
+    using value_type = float[];
+    using size_type = Kokkos::View<float * [nr_distr]>::size_type;
+
+    private:
+        view_type data_;
+
+    public:
+        size_type value_count;
+        explicit MultiSum(view_type data) : data_(data), value_count {data.extent(1)} {}
+        KOKKOS_INLINE_FUNCTION
+            void operator()(const size_type i, value_type result) const {
+                for (int j = 0; j < value_count; ++j) {
+                    result[j] += data_(i, j);
+                }
+            }
+        KOKKOS_INLINE_FUNCTION
+            void join(value_type result1, const value_type result2) const {
+                for (int j = 0; j < value_count; ++j) {
+                    result1[j] += result2[j];
+                }
+            }
+};
+
+int main(int argc, char* argv[]) {
+    Kokkos::initialize(argc, argv);
+    {
+        const int N {argc > 1 ? std::stoi(argv[1]) : 1'000};
+        const long seed {1234};
+        std::mt19937 engine(seed);
+        view_type data("data", N);
+        host_view_type host_data {Kokkos::create_mirror_view(data)};
+        for (int j = 0; j < nr_distr; ++j) {
+            std::uniform_real_distribution<float> distr(-3.0f + j, -1.0f + j);
+            for (int i = 0; i < N; ++i) {
+                host_data(i, j) = distr(engine);
+            }
+        }
+        Kokkos::deep_copy(data, host_data);
+        float result[nr_distr] {0.0f};
+        Kokkos::parallel_reduce(N, MultiSum(data), result);
+        for (int j = 0; j < nr_distr; ++j) {
+            std::cout << j << ": " << result[j]/N << "\n";
+        }
+    }
+    Kokkos::finalize();
+}
