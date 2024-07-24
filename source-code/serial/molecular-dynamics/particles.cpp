ed53195196345ca27e5ae4f869950ab422b3f1e8
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-07-24 14:02:24 +0200

    Change particle data layout

--- source-code/serial/molecular-dynamics/particles.cpp
+++ source-code/serial/molecular-dynamics/particles.cpp
@@ -2,24 +2,20 @@
 
 Particles::Particles(int nr_particles, double box_size, double sigma_velocity, unsigned long seed) :
     box_size_{box_size}, sigma_velocity_{sigma_velocity} {
-        x_.reserve(nr_particles);
-        y_.reserve(nr_particles);
-        z_.reserve(nr_particles);
-        vx_.reserve(nr_particles);
-        vy_.reserve(nr_particles);
-        vz_.reserve(nr_particles);
+        pos_.reserve(3*nr_particles);
+        vel_.reserve(3*nr_particles);
         std::mt19937 gen(seed);
         std::uniform_real_distribution<double> position_distr(-0.5f*box_size, 0.5f*box_size);
         for (int i = 0; i < nr_particles; ++i) {
-            x_.push_back(position_distr(gen));
-            y_.push_back(position_distr(gen));
-            z_.push_back(position_distr(gen));
+            pos_.push_back(position_distr(gen));
+            pos_.push_back(position_distr(gen));
+            pos_.push_back(position_distr(gen));
         }
         std::normal_distribution<double> velocity_distr(0.0f, sigma_velocity_);
         for (int i = 0; i < nr_particles; ++i) {
-            vx_.push_back(velocity_distr(gen));
-            vy_.push_back(velocity_distr(gen));
-            vz_.push_back(velocity_distr(gen));
+            vel_.push_back(velocity_distr(gen));
+            vel_.push_back(velocity_distr(gen));
+            vel_.push_back(velocity_distr(gen));
         }
     }
 
--- source-code/serial/molecular-dynamics/particles.h
+++ source-code/serial/molecular-dynamics/particles.h
@@ -8,21 +8,24 @@
 
 struct Particles {
     private:
-        std::vector<double> x_;
-        std::vector<double> y_;
-        std::vector<double> z_;
-        std::vector<double> vx_;
-        std::vector<double> vy_;
-        std::vector<double> vz_;
+        std::vector<double> pos_;
+        std::vector<double> vel_;
         const double box_size_;
         const double sigma_velocity_ {1.0f};
     public:
         Particles(int nr_particles, double box_size, double sigma_velocity, unsigned long seed);
-        const position_t position(size_t i) const { return {x_[i], y_[i], z_[i]}; }
-        void position(size_t i, const position_t& pos) { x_[i] = pos[0]; y_[i] = pos[1]; z_[i] = pos[2]; }
-        const velocity_t velocity(size_t i) const { return {vx_[i], vy_[i], vz_[i]}; }
-        void velocity(size_t i, const velocity_t& vel) { vx_[i] = vel[0]; vy_[i] = vel[1]; vz_[i] = vel[2]; }
-        size_t size() const { return x_.size(); }
+        const position_t position(size_t i) const { return {pos_[3*i], pos_[3*i + 1], pos_[3*i + 2]}; }
+        void position(size_t i, const position_t& pos) {
+            pos_[3*i] = pos[0];
+            pos_[3*i + 1] = pos[1];
+            pos_[3*i + 2] = pos[2];
+        }
+        const velocity_t velocity(size_t i) const { return {vel_[3*i], vel_[3*i + 1], vel_[3*i + 2]}; }
+        void velocity(size_t i, const velocity_t& vel) {
+            vel_[3*i] = vel[0];
+            vel_[3*i + 1] = vel[1];
+            vel_[3*i + 2] = vel[2]; }
+        size_t size() const { return pos_.size()/3; }
         double box_size() const { return box_size_; }
         double sigma_velocity() const { return sigma_velocity_; }
 };
