6f6493b46519ad0f04ff5c68f0b96ce9d1782571
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-07-24 08:56:14 +0200

    Declare default values in header
    It is easier to modify default values when they are declared
    in the header file rather than the implementation.

--- source-code/serial/molecular-dynamics/cli.cpp
+++ source-code/serial/molecular-dynamics/cli.cpp
@@ -9,17 +9,28 @@ command_line_args parse_command_line(int argc, char* argv[]) {
     // default values are for CO_2
     desc.add_options()
         ("help", "produce help message")
-        ("nr-particles", po::value<int>(&args.nr_particles)->default_value(10), "number of particles")
-        ("box-size", po::value<double>(&args.box_size)->default_value(1.0e-6f), "box size for initial positions (m)")
-        ("nr-steps", po::value<int>(&args.nr_steps)->default_value(10), "number of steps")
-        ("measure_steps", po::value<int>(&args.measure_steps)->default_value(1), "number of measurement steps")
-        ("delta-t", po::value<double>(&args.delta_t)->default_value(1.0e-9f), "time step (s)")
-        ("mass", po::value<double>(&args.mass)->default_value(7.3e-26f), "particle mass (kg)")
-        ("epsilon", po::value<double>(&args.epsilon)->default_value(256.0f*1.38e-23f), "Lennard-Jones epsilon (J)")
-        ("sigma", po::value<double>(&args.sigma)->default_value(0.37e-9f), "Lennard-Jones sigma (m)")
-        ("velocity_sigma", po::value<double>(&args.velocity_sigma)->default_value(3.2e2f), "velocity sigma (m/s)")
-        ("temperature", po::value<double>(&args.temperature)->default_value(300.0), "temperature (K)")
-        ("seed", po::value<unsigned long>(&args.seed)->default_value(1234), "random seed");
+        ("nr-particles", po::value<int>(&args.nr_particles)->default_value(args.nr_particles),
+         "number of particles")
+        ("box-size", po::value<double>(&args.box_size)->default_value(args.box_size),
+         "box size for initial positions (m)")
+        ("nr-steps", po::value<int>(&args.nr_steps)->default_value(args.nr_steps),
+         "number of steps")
+        ("measure_steps", po::value<int>(&args.measure_steps)->default_value(args.measure_steps),
+         "number of measurement steps")
+        ("delta-t", po::value<double>(&args.delta_t)->default_value(args.delta_t),
+         "time step (s)")
+        ("mass", po::value<double>(&args.mass)->default_value(args.mass),
+         "particle mass (kg)")
+        ("epsilon", po::value<double>(&args.epsilon)->default_value(args.epsilon),
+         "Lennard-Jones epsilon (J)")
+        ("sigma", po::value<double>(&args.sigma)->default_value(args.sigma),
+         "Lennard-Jones sigma (m)")
+        ("velocity_sigma", po::value<double>(&args.velocity_sigma)->default_value(args.velocity_sigma),
+         "velocity sigma (m/s)")
+        ("temperature", po::value<double>(&args.temperature)->default_value(args.temperature),
+         "temperature (K)")
+        ("seed", po::value<unsigned long>(&args.seed)->default_value(args.seed),
+         "random seed");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
--- source-code/serial/molecular-dynamics/cli.h
+++ source-code/serial/molecular-dynamics/cli.h
@@ -4,17 +4,17 @@
 #include <boost/program_options.hpp>
 
 struct command_line_args {
-    int nr_particles;
-    double box_size;
-    int nr_steps;
-    int measure_steps;
-    double delta_t;
-    double mass;
-    double epsilon;
-    double sigma;
-    double temperature;
-    double velocity_sigma;
-    unsigned long seed;
+    int nr_particles {100};
+    double box_size {1.0e-6};
+    int nr_steps {10};
+    int measure_steps {1};
+    double delta_t {1.0e-12};
+    double mass {7.3e-26};
+    double epsilon {256.0*1.38e-23};
+    double sigma {0.37e-9};
+    double temperature {300.0};
+    double velocity_sigma {3.2e2};
+    unsigned long seed {1234};
 };
 
 namespace po = boost::program_options;
