f3dc88b2790c7dc215e2ee4124c2ba92eaac61e5
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-07-30 16:54:34 +0200

    Add simple example using a lambda function

---
+++ source-code/kokkos/hello_world_lambdas.cpp
@@ -0,0 +1,11 @@
+#include <Kokkos_Core.hpp>
+#include <iostream>
+#include <typeinfo>
+
+int main(int argc, char* argv[]) {
+    Kokkos::initialize(argc, argv);
+    Kokkos::parallel_for(15, KOKKOS_LAMBDA(const int i) {
+            Kokkos::printf("Hello World from i = %d\n", i);
+            });
+    Kokkos::finalize();
+}
